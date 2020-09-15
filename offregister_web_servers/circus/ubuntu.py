9737d4f94012101b79910f1b49564ccbe0fcacb6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_web_servers/circus/ubuntu.py
+++ offregister_web_servers/circus/ubuntu.py
@@ -4,26 +4,40 @@ from fabric.contrib.files import upload_template
 
 
 def install(template_vars, local_tpl_dir):
-    sudo('pip2 install circus')
+    sudo("pip2 install circus")
 
     for filename in listdir(local_tpl_dir):
         name, ext = path.splitext(filename)
-        if name == 'circus':
+        if name == "circus":
             full_path = path.join(local_tpl_dir, filename)
-            if ext == '.conf':
-                upload_template(full_path, '/etc/init/circus.conf', context=template_vars, use_sudo=True)
-            elif ext == '.ini':
-                run('mkdir -p {remote_home}/conf'.format(remote_home=template_vars['HOME']))
-                upload_template(full_path, '{remote_home}/conf/circus.ini'.format(remote_home=template_vars['HOME']),
-                                context=template_vars)
+            if ext == ".conf":
+                upload_template(
+                    full_path,
+                    "/etc/init/circus.conf",
+                    context=template_vars,
+                    use_sudo=True,
+                )
+            elif ext == ".ini":
+                run(
+                    "mkdir -p {remote_home}/conf".format(
+                        remote_home=template_vars["HOME"]
+                    )
+                )
+                upload_template(
+                    full_path,
+                    "{remote_home}/conf/circus.ini".format(
+                        remote_home=template_vars["HOME"]
+                    ),
+                    context=template_vars,
+                )
 
-    sudo('initctl reload-configuration')
-    ''' dbus-send --system --print-reply --dest=com.ubuntu.Upstart
+    sudo("initctl reload-configuration")
+    """ dbus-send --system --print-reply --dest=com.ubuntu.Upstart
                   /com/ubuntu/Upstart/jobs/circus/_ org.freedesktop.DBus.Properties.GetAll string:''
-                  method return sender=:1.0 -> dest=:1.726 reply_serial=2 '''
-    if sudo('service circus status').endswith('stop/waiting'):
-        sudo('service circus start')
+                  method return sender=:1.0 -> dest=:1.726 reply_serial=2 """
+    if sudo("service circus status").endswith("stop/waiting"):
+        sudo("service circus start")
 
     # TODO: Write this to a logfile with a timestamp instead
-    run('mkdir -p $HOME/.setup')
-    run('touch $HOME/.setup/circus')
+    run("mkdir -p $HOME/.setup")
+    run("touch $HOME/.setup/circus")
--- setup.py
+++ setup.py
@@ -1,45 +1,47 @@
 from setuptools import setup, find_packages
 from os import path, listdir
 from functools import partial
-from itertools import imap, ifilter
 from ast import parse
 from distutils.sysconfig import get_python_lib
 
-if __name__ == '__main__':
-    package_name = 'offregister_web_servers'
+if __name__ == "__main__":
+    package_name = "offregister_web_servers"
 
     f_for = partial(path.join, path.dirname(__file__), package_name)
     d_for = partial(path.join, get_python_lib(), package_name)
 
-    nginx_data = partial(path.join, f_for('nginx', 'data'))
-    nginx_install_dir = partial(path.join, d_for('nginx', 'data'))
+    nginx_data = partial(path.join, f_for("nginx", "data"))
+    nginx_install_dir = partial(path.join, d_for("nginx", "data"))
 
-    get_vals = lambda var0, var1: imap(lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-                                       ifilter(lambda line: line.startswith(var0) or line.startswith(var1), f))
+    get_vals = lambda var0, var1: list(
+        map(
+            lambda buf: next([e.value.s for e in parse(buf).body]),
+            [line for line in f if line.startswith(var0) or line.startswith(var1)],
+        )
+    )
 
-    with open(path.join(package_name, '__init__.py')) as f:
-        __author__, __version__ = get_vals('__version__', '__author__')
+    with open(path.join(package_name, "__init__.py")) as f:
+        __author__, __version__ = get_vals("__version__", "__author__")
 
     setup(
         name=package_name,
         author=__author__,
         version=__version__,
-        description='Web Servers deployment module for Fabric (offregister)',
+        description="Web Servers deployment module for Fabric (offregister)",
         classifiers=[
-            'Development Status :: 7 - Inactive',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'License :: OSI Approved :: MIT License',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 2 :: Only'
+            "Development Status :: 7 - Inactive",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: MIT License",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
         ],
-        test_suite=package_name + '.tests',
+        test_suite=package_name + ".tests",
         packages=find_packages(),
         package_dir={package_name: package_name},
         data_files=[
-            (nginx_install_dir(), map(nginx_data, listdir(nginx_data()))),
-        ]
+            (nginx_install_dir(), list(map(nginx_data, listdir(nginx_data())))),
+        ],
     )
