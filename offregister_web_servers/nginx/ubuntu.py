6c548492683f6d50cf8b315d503550ef984873af
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_web_servers/nginx/ubuntu.py
+++ offregister_web_servers/nginx/ubuntu.py
@@ -1,35 +1,33 @@
 from os import path
 
-from fabric.api import put, sudo
-from fabric.contrib.files import upload_template
 from offregister_fab_utils.apt import apt_depends
 
 
 def install(local_conf_path, service_cmd="restart", name=None, template_vars=None):
-    apt_depends("nginx-full")
+    apt_depends(c, "nginx-full")
 
     remote_conf_name = name or path.basename(local_conf_path).partition(path.extsep)[0]
     remote_path = "/etc/nginx/sites-available/{remote_conf_name}".format(
         remote_conf_name=remote_conf_name
     )
 
-    sudo(
+    c.sudo(
         "rm -f /etc/nginx/sites-enabled/{remote_conf_name}".format(
             remote_conf_name=remote_conf_name
         )
     )
-    sudo("rm -f /etc/nginx/sites-enabled/default")
-    sudo("rm -f /etc/nginx/sites-available/default")
+    c.sudo("rm -f /etc/nginx/sites-enabled/default")
+    c.sudo("rm -f /etc/nginx/sites-available/default")
     if template_vars:
-        upload_template(
-            local_conf_path, remote_path, context=template_vars, use_sudo=True
+        upload_template_fmt(
+            c, local_conf_path, remote_path, context=template_vars, use_sudo=True
         )
     else:
-        put(local_conf_path, remote_path=remote_path, use_sudo=True)
-    # put(default_conf_path, '/etc/nginx/sites-enabled/default', use_sudo=True)
-    sudo(
+        c.put(local_conf_path, remote=remote_path, use_sudo=True)
+    # c.put(default_conf_path, '/etc/nginx/sites-enabled/default', use_sudo=True)
+    c.sudo(
         "ln -s /etc/nginx/sites-available/{remote_conf_name} /etc/nginx/sites-enabled/{remote_conf_name}".format(
             remote_conf_name=remote_conf_name
         )
     )
-    sudo("service nginx {service_cmd}".format(service_cmd=service_cmd))
+    c.sudo("service nginx {service_cmd}".format(service_cmd=service_cmd))
