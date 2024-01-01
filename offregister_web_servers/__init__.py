ba97b55029003a6030e7799d062e2976fd409286
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-01-01 18:23:37 -0500

    [setup.py] Fix Python 2.7 support ; standardise install scripts across all off* Python packages; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_web_servers/__init__.py
+++ offregister_web_servers/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 __author__ = "Samuel Marks"
 __version__ = "0.0.2"
-__description__ = "This package follows the offregister specification for web server setups"
+__description__ = (
+    "This package follows the offregister specification for web server setups"
+)
